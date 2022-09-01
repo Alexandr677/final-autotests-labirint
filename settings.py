20c3ed3fc39088e6e65f597f08d8f5097e29fd0c
Александр Горбачев <101926124+Alexandr677@users.noreply.github.com>
2022-09-02 02:09:29 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,23 @@
+valid_email = "alleksanderrr.gorbachyov@yandex.ru"
+valid_password = "jkbiksk88"
+project_url = 'https://www.labirint.ru/'
+favorites_url = 'https://www.labirint.ru/cabinet/putorder/'
+cart_url = 'https://www.labirint.ru/cart/'
+compare_url = 'https://www.labirint.ru/compare/'
+support_url = 'https://www.labirint.ru/support/'
+help_url = 'https://www.labirint.ru/help/'
+checkout_page = 'https://www.labirint.ru/basket/checkout/'
+discount_code = '565F-4519-8203'
+email = 'test@test.test'
+login = 'Александр'
+valid_phone = '+79043601277'
+
+social_network = {
+    'vk': 'https://vk.com/labirint_ru',
+    'vk.deti': 'https://vk.com/labirintdeti',
+    'youtube': 'https://www.youtube.com/user/labirintruTV',
+    'ok': 'https://ok.ru/labirintru',
+    'zen': 'https://zen.yandex.ru/labirintru',
+    'telegram': 'https://t.me/labirintru',
+    'tiktok': 'https://www.tiktok.com/@labirintru'
+}
